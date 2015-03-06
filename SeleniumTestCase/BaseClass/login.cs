4cfb75408dc39cc3100fd54f67bd0a7d9b990ea3
Sindhura <sindhurar@simplicityhealthsystems.com>
2015-03-06 13:40:36 -0600

    Url changed for server

--- SeleniumTestCase/BaseClass/login.cs
+++ SeleniumTestCase/BaseClass/login.cs
@@ -8,11 +8,11 @@ namespace SeleniumTestCase
 {
     public class login
     {
-        //public IWebDriver Driver { get; set; }
+       
             
             public static void loginmethod(IWebDriver driver, String userName, String pwd)
             {
-                driver.Navigate().GoToUrl("http://localhost:24129/");
+                driver.Navigate().GoToUrl("http://54.235.101.187:24129/");
                 driver.Manage().Window.Maximize();
                 driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(3000));
 
@@ -24,14 +24,14 @@ namespace SeleniumTestCase
              }
             public static void logoutmethod(IWebDriver driver)
             {
-                // Find an element and define it
-                IWebElement elementToClick = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/ul/li/a"));
-                // Scroll the browser to the element's Y position
-                (driver as IJavaScriptExecutor).ExecuteScript(string.Format("window.scrollTo(0, {0});", elementToClick.Location.Y));
-                // Click the element
-                elementToClick.Click();
+                //// Find an element and define it
+                //IWebElement elementToClick = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/ul/li/a"));
+                //// Scroll the browser to the element's Y position
+                //(driver as IJavaScriptExecutor).ExecuteScript(string.Format("window.scrollTo(0, {0});", elementToClick.Location.Y));
+                //// Click the element
+                //elementToClick.Click();
                 //Click dropdown to logout.
-                //driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/ul/li/a")).Click();
+                driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/ul/li/a")).Click();
                // Click logout.
                 driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/ul/li/ul/li[2]/a")).Click();
             
